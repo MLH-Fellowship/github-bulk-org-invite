86cf678e250a4e83f1479c30cc1b3a563558d6c4
Will Russell <will@mlh.io>
2023-01-16 17:21:27 +0000

    Fix env variable name

--- script.py
+++ script.py
@@ -12,14 +12,16 @@ with open("fellows.txt") as f:
         fellow_github = line.strip()
 
         # Get GitHub User ID from Username
-        r = requests.get(f"https://api.github.com/users/{fellow_github}")
-        user_id = r.json()["id"]
+        response = requests.get(f"https://api.github.com/users/{fellow_github}")
+        user_id = response.json()["id"]
         body = {
             "invitee_id": user_id
         }
 
         # Invite to org
-        r = requests.post(f"https://api.github.com/orgs/{os.getenv('GITHUB_ORG')}/invitations", json=body, auth=(os.getenv("GITHUB_USER"), os.getenv("GITHUB_ACCESS_TOKEN")))
-        print(r.json())
+        response = requests.post(url=f"https://api.github.com/orgs/{os.getenv('GITHUB_ORG')}/invitations", 
+                          json=body, 
+                          auth=(os.getenv("GITHUB_USERNAME"), os.getenv("GITHUB_ACCESS_TOKEN")))
+        print(f"{fellow_github}: {response.json()}")
         time.sleep(1)
 
